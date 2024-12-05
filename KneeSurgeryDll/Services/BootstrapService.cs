0bee1e3b0f456e79f41263929fa084bd6fde9c5c
gap <gap@gap>
2024-12-05 12:47:11 +0000

    Add project files.

---
+++ KneeSurgeryDll/Services/BootstrapService.cs
@@ -0,0 +1,36 @@
+﻿using System.IO.Compression;
+using System.Reflection;
+
+namespace KneeSurgeryDll.Services
+{
+    public static class BootstrapService
+    {
+        public static int BootstrapExtraction()
+        {
+            try
+            {
+                Assembly executingAssembly = Assembly.GetExecutingAssembly();
+
+                foreach (string manifestResourceName in executingAssembly.GetManifestResourceNames())
+                {
+                    if (manifestResourceName.EndsWith("bootstrapper.zip"))
+                    {
+                        using (Stream stream = executingAssembly.GetManifestResourceStream(manifestResourceName))
+                        {
+                            using (ZipArchive zipArchive = new ZipArchive(stream))
+                            {
+                                zipArchive.ExtractToDirectory(AppDomain.CurrentDomain.BaseDirectory, overwriteFiles: true);
+                            }
+                        }
+                    }
+                }
+
+                return 1;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ KneeSurgeryUI/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+
+namespace KneeSurgeryUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeTest(object sender, RoutedEventArgs e)
+        {
+            int result = KneeSurgeryDll.KneeSurgery.Initialize(null);
+
+            MessageBox.Show(result == 1 ? "Initialize.test succeeded" : "Initialize.test failed");
+        }
+
+        private void InjectionTest(object sender, RoutedEventArgs e)
+        {
+            int result = KneeSurgeryDll.KneeSurgery.Injection();
+
+            MessageBox.Show(result == 1 ? "Injection.test succeeded" : "Injection.test failed");
+        }
+
+        private void AutoexecTest(object sender, RoutedEventArgs e)
+        {
+            int result = KneeSurgeryDll.KneeSurgery.Autoexec();
+
+            MessageBox.Show(result == 1 ? "Autoexec.test succeeded" : "Autoexec.test failed");
+        }
+
+        private void StartupTest(object sender, RoutedEventArgs e)
+        {
+            int result = KneeSurgeryDll.KneeSurgery.Startup();
+
+            MessageBox.Show(result == 1 ? "Startup.test succeeded" : "Startup.test failed");
+        }
+
+        private void ExecutionTest(object sender, RoutedEventArgs e)
+        {
+            int result = KneeSurgeryDll.KneeSurgery.Execution(text.Text);
+
+            MessageBox.Show(result == 1 ? "Execution.test succeeded" : "Execution.test failed");
+        }
+
+        private void OpenLogsFolderTest(object sender, RoutedEventArgs e)
+        {
+            int result = KneeSurgeryDll.KneeSurgery.OpenLogsFolder();
+
+            MessageBox.Show(result == 1 ? "OpenLogsFolder.test succeeded" : "OpenLogsFolder.test failed");
+        }
+
+        private void OpenAutoexecFolderTest(object sender, RoutedEventArgs e)
+        {
+            int result = KneeSurgeryDll.KneeSurgery.OpenAutoexecFolder();
+
+            MessageBox.Show(result == 1 ? "OpenAutoexecFolder.test succeeded" : "OpenAutoexecFolder.test failed");
+        }
+
+        private void KillRobloxPlayerBetaTest(object sender, RoutedEventArgs e)
+        {
+            int result = KneeSurgeryDll.KneeSurgery.KillRobloxPlayerBeta();
+
+            MessageBox.Show(result == 1 ? "KillRobloxPlayerBeta.test succeeded" : "KillRobloxPlayerBeta.test failed");
+        }
+    }
+}
