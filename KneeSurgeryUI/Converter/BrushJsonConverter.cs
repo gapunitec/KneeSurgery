25b8ea4adac6a72021e921e9689fa9ff990024e8
gap <mp120805a@gmail.com>
2024-12-09 14:43:22 +0000

    Knee Surgery Dll & UI
    Themes

---
+++ KneeSurgeryUI/Converter/BrushJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Media;
+using Newtonsoft.Json;
+
+public class BrushJsonConverter : JsonConverter<Brush>
+{
+    public override void WriteJson(JsonWriter writer, Brush value, JsonSerializer serializer)
+    {
+        if (value is SolidColorBrush solidColorBrush)
+        {
+            string colorString = solidColorBrush.Color.ToString();
+            writer.WriteValue(colorString);
+        }
+        else
+        {
+            throw new NotSupportedException("Solo SolidColorBrush è supportato.");
+        }
+    }
+
+    public override Brush ReadJson(JsonReader reader, Type objectType, Brush existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        string colorString = (string)reader.Value;
+
+        if (ColorConverter.ConvertFromString(colorString) is Color color)
+        {
+            return new SolidColorBrush(color);
+        }
+
+        throw new JsonSerializationException($"Valore colore non valido: {colorString}");
+    }
+}
---
+++ KneeSurgeryUI/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Windows.Media;
+using Newtonsoft.Json;
+
+namespace KneeSurgeryUI
+{
+    public class Settings
+    {
+        public string ExplorerPath { get; set; }
+        public Theme SelectedTheme { get; set; }
+        public List<Theme> Themes { get; set; }
+
+        private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            Converters = new List<JsonConverter> { new BrushJsonConverter() }
+        };
+
+        public static Settings GetSettings()
+        {
+            return JsonConvert.DeserializeObject<Settings>(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings.json")), JsonSettings);
+        }
+
+        public void SetSettings()
+        {
+            File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings.json"), JsonConvert.SerializeObject(this, JsonSettings));
+        }
+    }
+
+    public class Theme
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Brush Foreground { get; set; }
+        public Brush Background { get; set; }
+        public Brush MainBackground { get; set; }
+    }
+}
